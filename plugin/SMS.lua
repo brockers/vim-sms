09289bcaf8e595ed453d7b3a631b6e961b7b8b10
Bobby Rockers <rrockers@gmail.com>
2024-03-30 12:18:28 -0500

    More testing.

--- plugin/SMS.lua
+++ plugin/SMS.lua
@@ -11,14 +11,20 @@ function CloseMenu()
   vim.api.nvim_win_close(Win_id, true)
 end
 
+function script_path()
+   local str = debug.getinfo(2, "S").source:sub(2)
+   return str:match("(.*/)")
+end
+
 function SMS(opts, cb)
   local height = 20
   local width = 30
   local borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" }
   local opts = {}
 
-  local file = io.open("shortcutlist.md", "r") -- Open the file in read mode
+  local file = io.open("./shortcutlist.md", "r") -- Open the file in read mode
 
+  print()
   if file then
     for line in file:lines() do
       table.insert(opts, line)
@@ -26,7 +32,8 @@ function SMS(opts, cb)
   end
 
   Win_id = popup.create(opts, {
-        title = "Custom Shortcuts",
+        -- title = "Custom Shortcuts",
+        title = script_path(),
         -- highlight = "MyProjectWindow",
         line = math.floor(((vim.o.lines - height) / 2) - 1),
         col = math.floor((vim.o.columns - width) / 2),
@@ -38,6 +45,7 @@ function SMS(opts, cb)
 
   local bufnr = vim.api.nvim_win_get_buf(Win_id)
   vim.api.nvim_buf_set_keymap(bufnr, "n", "q", "<cmd>lua CloseMenu()<CR>", { silent=false })
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "<Esc>", "<cmd>lua CloseMenu()<CR>", { silent=false })
 end
 
 vim.keymap.set("n", "<leader>h", '<cmd>lua SMS()<CR>')
